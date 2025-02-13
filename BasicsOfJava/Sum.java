52bc764efe861ed0a4bad5eeececc6fbad15c1ff
Shivam Shukla <ssinstitutes7776@gmail.com>
2025-02-13 18:31:26 +0000

    Sum of Numbers program

---
+++ BasicsOfJava/Sum.java
@@ -0,0 +1,10 @@
+//Sum of numbers in a variables
+public class Sum {
+    public static void main(String[] args) {
+        int x=3+5;
+        System.out.println(x);
+        // check datatype
+        System.out.println(((Object) x).getClass().getSimpleName());
+
+    }
+}
